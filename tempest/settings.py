0444c30ee8bd39e66adc4d2aa609407b5fec6a39
Kimathi  Sedegah <sedkim370@gmail.com>
2026-03-28 01:35:29 +0000

    cors

--- tempest/settings.py
+++ tempest/settings.py
@@ -16,6 +16,7 @@ if 'FLY_APP_NAME' in os.environ:
 INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'fileshare',
     'links',
@@ -24,6 +25,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -35,6 +37,11 @@ SESSION_COOKIE_SECURE = SECURE_SSL_REDIRECT
 CSRF_COOKIE_SECURE = SECURE_SSL_REDIRECT
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://tempest-u-oiya.fly.dev",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'tempest.urls'
 
