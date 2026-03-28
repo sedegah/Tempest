88d66b3cbda158a6f94ba8cc98e33de92728ddf2
Kimathi  Sedegah <sedkim370@gmail.com>
2026-03-28 01:47:22 +0000

    req

--- tempest/settings.py
+++ tempest/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
